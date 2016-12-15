4f1b207ce3c5faa21661eca39407f3ed42157c52
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-15 22:10:40 +0300

    Remove socket.io package

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const os = require('os');
 const request = require('request');
-const SocketIO = require('socket.io');
 require('dotenv').config();
 
 var app = express();
@@ -12,12 +11,3 @@ app.use('/', require('./routes'));
 const server = app
   .use(express.static(__dirname + '/public'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = SocketIO(server);
-// Attach the SocketIO server to the app object
-app.io = io;
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});

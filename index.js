4bfb5134f18410921d46c346746da640988de70f
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-19 14:35:05 +0300

    Install and configure body-parser

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const os = require('os');
-const request = require('request');
+const bodyParser = require('body-parser');
 const isProduction = process.env.NODE_ENV === 'production';
 
 if (!isProduction) require('dotenv').config();
@@ -9,7 +8,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 app.port = PORT;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json())
+  .use('/', require('./routes'))
+  .use(express.static(__dirname + '/public'));
 
 const server = app
   .use(express.static(__dirname + '/public'))

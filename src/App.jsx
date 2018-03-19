a860acb6d5e8659600e156fae9e8035a2fb2c0fd
Kevin Ndung'u <kevgathuku@gmail.com>
2018-03-19 10:51:31 +0300

    Use bulma for app styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,86 @@
-import React, { Component } from 'react';
-import {observer} from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
+import React, { Component } from "react";
 
-import actions from './actions';
-import utils from './utils';
-import TorrentStatus from './TorrentStatus.jsx';
+import actions from "./actions";
+import utils from "./utils";
+import TorrentStatus from "./TorrentStatus.jsx";
 
-const App = observer(class App extends Component {
+export default class App extends Component {
   render() {
     const store = this.props.store;
     return (
-      <div className="container">
-        <DevTools />
-        <div className="row">
-          <div className="one columns"><br></br></div>
-          <div className="nine columns">
-            <h3 className="center"> Save files to Dropbox </h3>
-            <form onSubmit={this.addTorrent}>
-              <label htmlFor="magnet_link">Magnet Link</label>
-              <input className="u-full-width" type="text" placeholder="magnet:?..." id="magnet_link" name="magnet"/>
-              <input className="button-primary" type="submit" value="Download"/>
-            </form>
-            {store.torrents.length > 0 ?
-              <TorrentStatus torrents={ store.torrents } />
-              : null}
-            </div>
+      <React.Fragment>
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <p className="title is-2 lit">Torrent to Dropbox</p>
+            <p className="subtitle is-5">
+              Dowload torrents straight to your Dropbox
+            </p>
           </div>
-      </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-8 is-offset-2">
+            <form onSubmit={this.addTorrent}>
+              <div class="field">
+                <p class="control has-icon">
+                  <input
+                    class="input is-primary"
+                    id="magnet_link"
+                    name="magnet"
+                    placeholder="Enter magnet URI"
+                    type="text"
+                  />
+                  <span class="icon is-small">
+                    <i class="fa fa-magnet" aria-hidden="true" />
+                  </span>
+                </p>
+              </div>
+              <div class="form-group">
+                <button class="button is-medium is-primary" type="submit">
+                  Download
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        {store.torrents.length > 0 ? (
+          <TorrentStatus torrents={store.torrents} />
+        ) : null}
+      </React.Fragment>
     );
   }
 
   componentDidMount() {
-    actions.socket.on('download:start', this.torrentDownloadStart);
-    actions.socket.on('download:progress', this.torrentDownloadProgress);
-    actions.socket.on('download:complete', this.torrentDownloadComplete);
+    actions.socket.on("download:start", this.torrentDownloadStart);
+    actions.socket.on("download:progress", this.torrentDownloadProgress);
+    actions.socket.on("download:complete", this.torrentDownloadComplete);
   }
 
-  torrentDownloadStart = (torrent) => {
+  torrentDownloadStart = torrent => {
     const store = this.props.store;
     store.addTorrent(torrent);
-  }
+  };
 
-  torrentDownloadProgress = (torrent) => {
+  torrentDownloadProgress = torrent => {
     const store = this.props.store;
     store.mergeTorrentInfo(torrent);
-  }
+  };
 
-  torrentDownloadComplete = (torrent) => {
+  torrentDownloadComplete = torrent => {
     const store = this.props.store;
     store.mergeTorrentInfo(torrent);
-  }
+  };
 
-  addTorrent = (event) => {
+  addTorrent = event => {
     // Prevent the default action for form submission
     event.preventDefault();
     const store = this.props.store;
     // Get the form object and extract the FormData from it
     // This returns the value of the magnet field
-    var magnetValue = new FormData(event.target).get('magnet');
-    if (!(utils.isValidMagnetURI(magnetValue))) {
-      return store.setStatus('Please enter a valid magent URI');
+    var magnetValue = new FormData(event.target).get("magnet");
+    if (!utils.isValidMagnetURI(magnetValue)) {
+      return store.setStatus("Please enter a valid magent URI");
     }
     actions.postMagnetURI(store, magnetValue);
-  }
-});
-
-export default App;
+  };
+}

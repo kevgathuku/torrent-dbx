0fc67387d9519300b5f10b51f2b76f7575cd6e72
Kevin Ndung'u <kevgathuku@gmail.com>
2020-04-06 16:12:58 +0300

    Remove sentry

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "bulma/css/bulma.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'bulma/css/bulma.css';
 
-import App from "./App";
-import { store } from "./reducer";
-
-window.Raven.config(
-  "https://f5427ee402e8415bbe7475afc3551eb8@sentry.io/146783"
-).install();
+import App from './App';
+import { store } from './reducer';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 store.subscribe(() => {
